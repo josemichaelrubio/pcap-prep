37e0d1119a0c3fd3e461d1e4204a46ae29abce2f
Jose Michael Rubio <contact@josemichaelrubio.com>
2024-03-02 22:41:55 -0500

    Completed _3-math-module.py

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -1,3 +1,7 @@
 
 _
 /_1-Modules_and_Packages/_2-importing_modules.py,c/6/c6687f935451d8688d8e788f57d493b6cb452a9e
+V
+&_1-Modules_and_Packages/_3-math-module,3/1/31779003dcf1c4eea806ea38f7f3e331ea429df3
+Y
+)_1-Modules_and_Packages/_3-math-module.py,f/d/fd381a9d776066e37142daa76e9c2c0f252f5d7e
---
+++ _1-Modules_and_Packages/_3-math-module.py
@@ -0,0 +1,30 @@
+import math
+
+# how to print all the entities in a module
+for name in dir(math):
+    print(name, end='\t')
+
+# For the PCAP, you should know the following functions: ceil, floor, trunc, factorial, sqrt, hypot
+
+# math.ceil(x) - returns the smallest integer not less than x
+print(math.ceil(3.14)) # =4
+print(math.ceil(-3.14)) # =-3
+
+# math.floor(x) - returns the largest integer not greater than x
+print(math.floor(3.14)) # =3
+print(math.floor(-3.14)) # =-4
+
+# math.trunc(x) - returns the integer part of x (remove the decimal part)
+print(math.trunc(3.14)) # =3
+print(math.trunc(-3.14)) # =-3
+
+# math.factorial(x) - returns the factorial of x= multiplication of all integers less than or equal to x
+print(math.factorial(5)) # =120
+
+# math.sqrt(x) - returns the square root of x. If x is negative, it returns a ValueError. Note that the result is a float
+print(math.sqrt(16)) # =4.0
+
+# math.hypot(x, y) - returns the length of the hypotenuse of a right-angled triangle with the legs of length x and y
+print(math.hypot(3, 4)) # =5.0
+
+
