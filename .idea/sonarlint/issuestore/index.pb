1daa8287cb3fec1610268ab4787e2c7bb6357adb
Jose Michael Rubio <contact@josemichaelrubio.com>
2024-03-02 23:03:13 -0500

    Added _1-intro_to_modules.py

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -7,3 +7,7 @@ Y
 )_1-Modules_and_Packages/_3-math-module.py,f/d/fd381a9d776066e37142daa76e9c2c0f252f5d7e
 [
 +_1-Modules_and_Packages/_4-random-module.py,e/3/e3e6ef753770558c91fa3616d12a354caa792a69
+[
++_1-Modules_and_Packages/intro_to_modules.py,7/a/7a4629fc81284f42c046a960a27502b5a80bd10e
+^
+._1-Modules_and_Packages/_1-intro_to_modules.py,6/1/611f3e50104c61c6b331b037e6a580e640321e21
---
+++ _1-Modules_and_Packages/_1-intro_to_modules.py
@@ -0,0 +1,11 @@
+"""
+Decomposition- breaking down code into smaller pieces, stored into separate files
+
+Modules- a file that contains python definitions and statements
+	• Can be imported into a second file
+	• when imported,  you can use the modules from the first file in the second file
+	• You can create your own modules or other user modules
+		○ Modules can be found at Python Standard Library
+Within the modules, you can find the entities-all the definitions (variables and methods) that you can use
+
+"""
