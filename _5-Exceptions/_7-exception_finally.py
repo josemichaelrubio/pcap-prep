483638612bd46f81641dde35fbc344481ac8e101
Jose Michael Rubio <contact@josemichaelrubio.com>
2024-03-27 13:56:00 -0400

    _7-exception_finally.py completed

--- _5-Exceptions/_7-exception_finally.py
+++ _5-Exceptions/_7-exception_finally.py
@@ -1,3 +1,22 @@
 """
+*The finally Keyword
+*====================
+`finally:` must be the last branch of the code intended for handling exceptions, even after else.
+	• else and finally are not dependent on each other
+
+General rule, the finally block is always executed
+
+Typically used when working on external resources such as databases or text files
 
 """
+
+def get_inverse(x):
+    try:
+        return 1/x
+    except ZeroDivisionError:
+        print ('There was an ZeroDivisionError')
+    finally:
+        print('I am always printed!')
+
+print(get_inverse(5))
+print(get_inverse(0))
