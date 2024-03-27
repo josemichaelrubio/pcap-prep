f7d7786a862d96286ca732365d40edccde03e819
Jose Michael Rubio <contact@josemichaelrubio.com>
2024-03-27 11:11:23 -0400

    Added _5-Exception directory and two .py files that are completed

---
+++ _5-Exceptions/_0-regards_to_exam.py
@@ -0,0 +1,9 @@
+"""
+An important piece of information before you start the exceptions section:
+
+Previously, exceptions were only part of the PCAP exam (Associate level) and were completely absent from the PCEP exam (Entry level). However, a 2022 change to the Entry-level exam syllabus introduced exceptions basics to the syllabus.
+
+In theory, the syllabuses of PCEP and PCAP should be different: PCAP should test new, more advanced topics that PCEP doesn't. However, in case of exceptions, it seems that both exams require the knowledge of exceptions basics. I also got a written confirmation from the Python Institute that this is the case: even though PCEP now verifies your exception basics knowledge, the same kinds of questions can reappear in the more advanced PCAP exam.
+
+That's why the first four videos in this section are marked (PCEP&PCAP). These four videos are almost identical with my PCEP course. If you feel like you already know everything from PCEP, you can simply skip these videos and move on to the videos about more advanced exceptions concepts.
+"""
---
+++ _5-Exceptions/_1-intro_exceptions.py
@@ -0,0 +1,32 @@
+"""
+* Introduction to Exceptions
+*===========================*
+Exceptions = an event which occurs during the execution of a program that disrupts the normal flow of the program instructions.
+
+Syntax Errors like IndentationError
+Value Errors
+
+Python does not know what to do with exceptions, thus it raises an exception and stops the program
+
+To handle exceptions, we use a try, exception structure
+
+Gather exceptions that will occur, then add them to the exception blocks below the try
+	• None of the exceptions can be repeated in the except blocks
+	• If none of the exceptions covers the error that occurred, the program will stop
+		○ Default Exception = must use the last general except block for all other exceptions not named in the blocks above that
+
+"""
+
+try: 
+    # Where the error might occur
+    value = int(input("Enter an integer:"))
+    value = 1/value
+    print(value)
+except ValueError:
+    # What to do if the error occurs
+    print("Invalid input. Please enter a number.")
+except ZeroDivisionError:
+    print("Cannot divide by zero.")
+except:
+    print("An unknown error occurred.")
+
